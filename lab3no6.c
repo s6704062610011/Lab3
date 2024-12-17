342ad35eb43d0aceddab6df4012494df4c04ee76
s6704062610011 <s6704062610011@email.kmutnb.ac.th>
2024-12-17 14:51:35 +0700

    Add files via upload

--- lab3no6.c
+++ lab3no6.c
@@ -1,35 +1,94 @@
 #include <stdio.h>
-
 int main()
 {
-    int score;
-    printf("Enter score:");
-    scanf("%d", &score);
-    if (score >= 68)
-    {
-        if (score >= 85)
-        {
-            printf("You got grade: A");
-        }
-        else if (score >= 75)
-        {
-            printf("You got grade: B");
-        }
-        else if (score >= 68)
-        {
-            printf("You got grade: C");
-        }        
-    }
-    else
-    {
-        if (score >= 50)
-        {
-            printf("You got grade: D");
-        }
-        else
-        {
-            printf("You got grade: F");
-        }
-    }
-    return 0;
-}
+	 int score;
+	 char a;
+	 printf("Enter your score: ");
+	 scanf("%d", &score);
+	 if (score >= 0)
+	 {
+	 	if (score <= 100)
+	 	{
+	 		if (score >= 68)
+	 		{
+	 			if (score >= 85)
+	 			{
+	 				printf("A\n");
+	 			}
+	 			else if (score >= 75)
+	 			{
+	 				printf("B\n");
+	 			}
+		 		else if (score >= 68)
+		 		{
+		 			printf("C\n");
+		 			printf("%d more score for grade B\n", 75 - score);
+		 		}
+		 	}
+		 	else
+		 	{
+		 		if (score >= 55)
+		 		{
+		 		printf("D\n");
+		 		}
+		 		else
+		 		{
+		 			printf("F\n");
+		 		}
+		 	}
+		 }
+		 else
+		 {
+		 	printf("error score\n");
+		 }
+		 }
+		 else
+		 {
+		 printf("error score\n");
+		 }
+		 printf("Enter your score: ");
+		 scanf("%d", &score);
+		 if (score >= 0)
+		{
+		 	if (score <= 100)
+		 	{
+		 		if (score < 68)
+		 		{
+		 			if (score >= 55)
+		 			{
+		 				printf("D\n");
+		 			}
+		 			else
+		 			{
+		 				printf("F\n");
+		 			}
+		 		}
+		 		else
+		 		{
+					if (score >= 85)
+					 {
+					 	printf("A\n");
+					 }
+					 else if (score >= 75)
+					 {
+					 	printf("B\n");
+					 }
+					 else if (score >= 68)
+					 {
+					 	printf("C\n");
+					 	printf("%d more score for grade B\n", 75 - score);
+					 }
+		 		}
+		 	}
+		 	else
+		 	{
+		 		printf("error score\n");
+		 	}
+		}
+		 else
+		 {
+		 	printf("error score\n");
+		 }
+	 return 0;
+} 
+
